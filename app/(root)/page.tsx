2b6854eb56842e65fd0147650428629be4b41e6b
Rahul NS <rahul9380767545@gmail.com>
2026-03-22 10:31:40 +0530

    feat(nav): add mobile sidebar with navigation links

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,4 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/route";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
@@ -9,13 +7,7 @@ const Home = async () => {
 
   return (
     <>
-      <form className="px-10 pt-25" action={async () => {
-        "use server";
-
-        await signOut({redirectTo: ROUTES.SIGN_IN });
-      }}>
-        <Button className="cursor-pointer" type="submit">Logout</Button>
-      </form>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
     </>
   );
 };
