a1338a51cf336fa71e38b3c8068f98e945734379
Rahul NS <rahul9380767545@gmail.com>
2026-04-21 18:12:56 +0530

    feat(user): implement user API with GET and POST endpoints

---
+++ app/database.ts
@@ -0,0 +1,46 @@
+const tickets = [
+    {
+        id: 1,
+        name: 'Fix next.js API Route bug',
+        status: 'open',
+        type: 'bug',
+    },
+    {
+        id: 2,
+        name: 'Add a new feature to the app',
+        status: 'open',
+        type: 'feature',
+    },
+    {
+        id: 3,
+        name: 'Refactor the app',
+        status: 'open',
+        type: 'refactor',
+    },
+    {
+        id: 4,
+        name: 'Fix a bug in the app',
+        status: 'open',
+        type: 'bug',
+    },
+    {
+        id: 5,
+        name: 'Add a new feature to the app',
+        status: 'open',
+        type: 'feature',
+    },
+    {
+        id: 6,
+        name: 'Refactor the app',
+        status: 'open',
+        type: 'refactor',
+    },
+    {
+        id: 7,
+        name: 'Fix a bug in the app',
+        status: 'open',
+        type: 'bug',
+    },
+];
+
+export default tickets;
