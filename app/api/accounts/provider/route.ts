c0cc23a34d31ec10bcef04c6cd67881d077c137c
Rahul NS <rahul9380767545@gmail.com>
2026-04-24 21:07:59 +0530

    feat(account): add endpointto fetch account by providerAccountId

---
+++ app/api/accounts/provider/route.ts
@@ -0,0 +1,24 @@
+import { APIErrorResponse } from "@/app/types/global";
+import Account from "@/database/account.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import { AccountSchema } from "@/lib/validations";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+    try {
+        const { providerAccountId } = await request.json();
+
+        const validatedData = AccountSchema.partial().safeParse({ providerAccountId });
+
+        if (!validatedData.success)
+            throw new ValidationError(validatedData.error.flatten().fieldErrors);
+
+        const account = await Account.findOne({ providerAccountId });
+        if (!account) throw new NotFoundError('Account');
+
+        return NextResponse.json({ success: true, data: account }, { status: 200 });
+    } catch (error) {
+        return handleError(error, 'api') as APIErrorResponse;
+    }
+}
