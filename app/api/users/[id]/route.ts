1afac2dbad71130e069bffcd772fa9a2400e59f2
Rahul NS <rahul9380767545@gmail.com>
2026-04-24 13:44:23 +0530

    fix(user): return 200 instead of 204 for delete endpoint

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -7,6 +7,7 @@ import { UserSchema } from "@/lib/validations";
 import { isValidObjectId } from "mongoose";
 import { NextResponse } from "next/server";
 
+// get users
 export async function GET(_: Request, { params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
 
@@ -19,12 +20,12 @@ export async function GET(_: Request, { params }: { params: Promise<{ id: string
         if (!user) throw new NotFoundError('User');
 
         return NextResponse.json({ success: true, data: user }, { status: 200 });
-
     } catch (error) {
         return handleError(error, 'api') as APIErrorResponse;
     }
 }
 
+// delete users
 export async function DELETE(_: Request, { params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
 
@@ -36,12 +37,13 @@ export async function DELETE(_: Request, { params }: { params: Promise<{ id: str
         const user = await User.findByIdAndDelete(id);
         if (!user) throw new NotFoundError('User');
 
-        return NextResponse.json({ success: true, data: user }, { status: 204 });
+        return NextResponse.json({ success: true, data: user}, { status: 200 });
     } catch (error) {
         return handleError(error, 'api') as APIErrorResponse;
     }
 }
 
+// update users
 export async function PUT(request: Request, { params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
 
@@ -49,7 +51,6 @@ export async function PUT(request: Request, { params }: { params: Promise<{ id: 
         await dbConnect();
 
         if (!id || !isValidObjectId(id)) throw new NotFoundError('User');
-
         const body = await request.json();
         const validatedData = UserSchema.partial().parse(body);
 
