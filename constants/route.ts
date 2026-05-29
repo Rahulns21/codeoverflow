4bd33e92af77615935a7684ee2ee3299d2a465da
Rahul NS <rahul9380767545@gmail.com>
2026-05-29 18:52:43 +0530

    feat(ui): add reusable data renderer for loading and error states

--- constants/route.ts
+++ constants/route.ts
@@ -1,12 +1,16 @@
 const ROUTES = {
-    HOME: '/',
-    SIGN_IN: '/signin',
-    SIGN_UP: '/signup',
-    PROFILE: (id: string) => `/profile/${id}`,
-    QUESTION: (id: string) => `/questions/${id}`,
-    TAGS: (id: string) => `/tags/${id}`,
-    ASK_QUESTION: "/ask-question",
-    SIGN_IN_WITH_OAUTH: "/signin-with-oauth",
-}
+  HOME: "/",
+  SIGN_UP: "/signup",
+  SIGN_IN: "/signin",
+  COLLECTION: "/collection",
+  COMMUNITY: "/community",
+  TAGS: "/tags",
+  JOBS: "/jobs",
+  PROFILE: (id: string) => `/profile/${id}`,
+  QUESTION: (id: string) => `/questions/${id}`,
+  TAG: (id: string) => `/tags/${id}`,
+  ASK_QUESTION: "/ask-question",
+  SIGN_IN_WITH_OAUTH: "/signin-with-oauth",
+};
 
 export default ROUTES;
---
+++ constants/states.ts
@@ -0,0 +1,49 @@
+import ROUTES from "./route";
+
+export const DEFAULT_EMPTY = {
+  title: "No Data Found",
+  message:
+    "Looks like the database is taking a nap. Wake it up with some new entries.",
+  button: {
+    text: "Add Data",
+    href: ROUTES.HOME,
+  },
+};
+
+export const DEFAULT_ERROR = {
+  title: "Oops! Something went wrong",
+  message: "Even our code can have a bad day. Give it another shot.",
+  button: {
+    text: "Try again",
+    href: ROUTES.HOME,
+  },
+};
+
+export const EMPTY_QUESTION = {
+  title: "Ahh, No Questions Yet!",
+  message:
+    "The question board is empty. Maybe it's waiting for you to ask something.",
+  button: {
+    text: "Ask a question",
+    href: ROUTES.ASK_QUESTION,
+  },
+};
+
+export const EMPTY_TAGS = {
+  title: "No Tags Found",
+  message: "The tag cloud is empty. Add some keywords to make it rain?",
+  button: {
+    text: "Create Tag",
+    href: ROUTES.TAGS,
+  },
+};
+
+export const EMPTY_COLLECTIONS = {
+  title: "Collections Are Empty",
+  message:
+    "Looks like you haven't created any collections yet. Start curatinf something extraordinary today",
+  button: {
+    text: "Save to Collection",
+    href: ROUTES.COLLECTION,
+  },
+};
