3721763ed99f797ea938f86562b62201511a9ac6
Rahul NS <rahul9380767545@gmail.com>
2026-06-04 11:12:23 +0530

    feat(questions): implement view increment on question details page

--- app/types/action.d.ts
+++ app/types/action.d.ts
@@ -1,39 +1,43 @@
 import { PaginatedSearchParams } from "./global";
 
-export type Provider = 'github' | 'google';
+type Provider = "github" | "google";
 
-export interface SignInWithOAuthParams {
-    provider: Provider;
-    providerAccountId: string;
-    user: {
-        email: string;
-        name: string;
-        image: string;
-        username: string;
-    }
-}
-
-export interface AuthCredentials {
-    name: string;
-    username: string;
+interface SignInWithOAuthParams {
+  provider: Provider;
+  providerAccountId: string;
+  user: {
     email: string;
-    password: string;
+    name: string;
+    image: string;
+    username: string;
+  };
 }
 
-export interface CreateQuestionParams {
-    title: string;
-    content: string;
-    tags: string[];
+interface AuthCredentials {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
 }
 
-export interface EditQuestionParams extends CreateQuestionParams {
-    questionId: string;
+interface CreateQuestionParams {
+  title: string;
+  content: string;
+  tags: string[];
 }
 
-export interface GetQuestionParams {
-    questionId: string;
+interface EditQuestionParams extends CreateQuestionParams {
+  questionId: string;
 }
 
-export interface GetTagQuestionsParams extends Omit<PaginatedSearchParams, "filter"> {
-    tagId: string;
+interface GetQuestionParams {
+  questionId: string;
+}
+
+interface GetTagQuestionsParams extends Omit<PaginatedSearchParams, "filter"> {
+  tagId: string;
+}
+
+interface IncrementViewsParams {
+  questionId: string;
 }
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -4,6 +4,7 @@ import {
   CreateQuestionParams,
   EditQuestionParams,
   GetQuestionParams,
+  IncrementViewsParams,
 } from "@/app/types/action";
 import {
   ActionResponse,
@@ -16,6 +17,7 @@ import {
   AskQuestionSchema,
   EditQuestionSchema,
   GetQuestionSchema,
+  IncrementViewsSchema,
   PaginatedSearchParamsSchema,
 } from "../validations";
 import handleError from "../handlers/error";
@@ -23,6 +25,8 @@ import mongoose, { QueryFilter } from "mongoose";
 import Question, { IQuestionDoc } from "@/database/question.model";
 import Tag, { ITagDoc } from "@/database/tag.model";
 import TagQuestion from "@/database/tag-question.model";
+import { revalidatePath } from "next/cache";
+import ROUTES from "@/constants/route";
 
 export async function createQuestion(
   params: CreateQuestionParams
@@ -296,6 +300,39 @@ export async function getQuestions(
       success: true,
       data: { questions: JSON.parse(JSON.stringify(questions)), isNext },
     };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function incrementViews(
+  params: IncrementViewsParams
+): Promise<ActionResponse<{ views: number }>> {
+  const validationResult = await action({
+    params,
+    schema: IncrementViewsSchema
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { questionId } = validationResult.params!;
+
+  try {
+    const question = await Question.findById(questionId);
+
+    if (!question) throw new Error("Question not found");
+
+    question.views += 1;
+
+    await question.save();
+    revalidatePath(ROUTES.QUESTION(questionId));
+
+    return {
+      success: true,
+      data: { views: question.views }
+    }
   } catch (error) {
     return handleError(error) as ErrorResponse;
   }
