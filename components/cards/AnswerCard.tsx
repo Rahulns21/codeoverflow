87d5ee7849d7a3c7356768d8cef48202170a5c02
Rahul NS <rahul9380767545@gmail.com>
2026-07-20 10:06:58 +0530

    feat(vote): implement voting to answers

--- components/cards/AnswerCard.tsx
+++ components/cards/AnswerCard.tsx
@@ -6,8 +6,23 @@ import { Answer } from "@/app/types/global";
 import UserAvatar from "../UserAvatar";
 import ROUTES from "@/constants/route";
 import Preview from "../editor/Preview";
+import { Suspense } from "react";
+import Votes from "../votes/Votes";
+import { hasVoted } from "@/lib/actions/vote.action";
 
-const AnswerCard = ({ _id, author, content, createdAt }: Answer) => {
+const AnswerCard = ({
+  _id,
+  author,
+  content,
+  createdAt,
+  upvotes,
+  downvotes,
+}: Answer) => {
+  const hasVotedPromise = hasVoted({
+    targetId: _id,
+    targetType: "answer",
+  });
+
   return (
     <article className="light-border border-b py-10">
       <span id={JSON.stringify(_id)} className="hash-span" />
@@ -29,14 +44,24 @@ const AnswerCard = ({ _id, author, content, createdAt }: Answer) => {
               {author.name ?? "Anonymous"}
             </p>
 
-            <p className="small-regular text-light400_light500 ml-0.5 mt-0.5 line-clamp-1">
+            <p className="small-regular text-light400_light500 mt-0.5 ml-0.5 line-clamp-1">
               <span className="max-sm:hidden"> • </span>
               answered {getTimeStamp(createdAt)}
             </p>
           </Link>
         </div>
 
-        <div className="flex justify-end">Votes</div>
+        <div className="flex justify-end">
+          <Suspense fallback={<div>Loading...</div>}>
+            <Votes
+              targetType={"answer"}
+              targetId={_id}
+              hasVotedPromise={hasVotedPromise}
+              upvotes={upvotes}
+              downvotes={downvotes}
+            />
+          </Suspense>
+        </div>
       </div>
 
       <Preview content={content} />
