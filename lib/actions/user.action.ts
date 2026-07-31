41b293b2015a14aa41d6539975bfc2c085c5a706
Rahul NS <rahul9380767545@gmail.com>
2026-07-31 12:52:47 +0530

    feat(users): add getUserProfile server action

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,74 +1,121 @@
-'use server';
+"use server";
 
-import { ActionResponse, ErrorResponse, PaginatedSearchParams, User as Users } from "@/app/types/global";
+import {
+  ActionResponse,
+  ErrorResponse,
+  PaginatedSearchParams,
+  User as UserParams,
+} from "@/app/types/global";
 import action from "../handlers/action";
-import { PaginatedSearchParamsSchema } from "../validations";
+import { GetUserSchema, PaginatedSearchParamsSchema } from "../validations";
 import handleError from "../handlers/error";
-import { User } from "@/database";
+import { Answer, Question, User } from "@/database";
 import { QueryFilter } from "mongoose";
+import { GetUserParams } from "@/app/types/action";
 
-export async function getUsers(params: PaginatedSearchParams): Promise<ActionResponse<{ users: Users[], isNext: boolean }>> {
-    const validationResult = await action({
-        params,
-        schema: PaginatedSearchParamsSchema,
-    });
+export async function getUsers(
+  params: PaginatedSearchParams
+): Promise<ActionResponse<{ users: UserParams[]; isNext: boolean }>> {
+  const validationResult = await action({
+    params,
+    schema: PaginatedSearchParamsSchema,
+  });
 
-    if (validationResult instanceof Error) {
-        return handleError(validationResult) as ErrorResponse;
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { page = 1, pageSize = 10, query, filter } = params;
+
+  const skip = (Number(page) - 1) * pageSize;
+  const limit = pageSize;
+
+  const filterQuery: QueryFilter<typeof User> = {};
+
+  if (query) {
+    filterQuery.$or = [
+      { name: { $regex: query, $options: "i" } },
+      { email: { $regex: query, $options: "i" } },
+    ];
+  }
+
+  let sortCriteria = {};
+
+  switch (filter) {
+    case "newest":
+      sortCriteria = { createdAt: -1 };
+      break;
+
+    case "oldest":
+      sortCriteria = { createdAt: 1 };
+      break;
+
+    case "popular":
+      sortCriteria = { reputation: -1 };
+      break;
+
+    default:
+      sortCriteria = { createdAt: -1 };
+      break;
+  }
+
+  try {
+    const totalUsers = await User.countDocuments(filterQuery);
+
+    const users = await User.find(filterQuery)
+      .sort(sortCriteria)
+      .skip(skip)
+      .limit(limit);
+
+    const isNext = totalUsers > skip + users.length;
+
+    return {
+      success: true,
+      data: {
+        users: JSON.parse(JSON.stringify(users)),
+        isNext,
+      },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function getUser(params: GetUserParams): Promise<
+  ActionResponse<{
+    user: UserParams;
+    totalQuestions: number;
+    totalAnswers: number;
+  }>
+> {
+  const validationResult = await action({
+    params,
+    schema: GetUserSchema,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { userId } = params;
+
+  try {
+    const user = await User.findById(userId).lean();
+
+    if (!user) throw new Error("User not found");
+
+    const totalQuestions = await Question.countDocuments({ author: userId });
+    const totalAnswers = await Answer.countDocuments({ author: userId });
+
+    return {
+        success: true,
+        data: {
+            user,
+            totalQuestions,
+            totalAnswers
+        }
     }
-
-    const { page = 1, pageSize = 10, query, filter } = params;
-
-    const skip = (Number(page) - 1) * pageSize;
-    const limit = pageSize;
-
-    const filterQuery: QueryFilter<typeof User> = {};
-
-    if (query) {
-        filterQuery.$or = [
-            { name: { $regex: query, $options: "i" } },
-            { email: { $regex: query, $options: "i" } },
-        ]
-    }
-
-    let sortCriteria = {};
-
-    switch (filter) {
-        case 'newest':
-            sortCriteria = { createdAt: -1 };
-            break;
-
-        case 'oldest':
-            sortCriteria = { createdAt: 1 };
-            break;
-
-        case 'popular':
-            sortCriteria = { reputation: -1 };
-            break;
-
-        default:
-            sortCriteria = { createdAt: -1 };
-            break;
-    }
-
-    try {
-        const totalUsers = await User.countDocuments(filterQuery);
-
-        const users = await User.find(filterQuery)
-            .sort(sortCriteria)
-            .skip(skip)
-            .limit(limit);
-
-            const isNext = totalUsers > skip + users.length;
-
-            return {
-                success: true,
-                data: {
-                    users: JSON.parse(JSON.stringify(users)),
-                    isNext,
-                }
-            }
-    } catch (error) {
-        return handleError(error) as ErrorResponse;
-    }
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
 }
