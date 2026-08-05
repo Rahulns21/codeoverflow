1e3a460a3aacb53c9d512ae77e5b7265b924a5a8
Rahul NS <rahul9380767545@gmail.com>
2026-08-05 16:48:37 +0530

    feat(profile): implement Tabs layout

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -8,6 +8,7 @@ import dayjs from "dayjs";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Stats from "@/components/user/Stats";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Profile = async ({ params }: RouteParams) => {
   const { id } = await params;
@@ -41,59 +42,89 @@ const Profile = async ({ params }: RouteParams) => {
 
   return (
     <>
-    <section className="flex flex-col-reverse items-start justify-between sm:flex-row">
-      <div className="flex flex-col items-start gap-4 lg:flex-row">
-        <UserAvatar
-          id={_id}
-          name={name}
-          imageUrl={image}
-          className="size-35 rounded-full object-cover"
-          fallbackClassname="text-6xl font-bolder"
-        />
+      <section className="flex flex-col-reverse items-start justify-between sm:flex-row">
+        <div className="flex flex-col items-start gap-4 lg:flex-row">
+          <UserAvatar
+            id={_id}
+            name={name}
+            imageUrl={image}
+            className="size-35 rounded-full object-cover"
+            fallbackClassname="text-6xl font-bolder"
+          />
 
-        <div className="mt-3">
-          <h2 className="h2-bold text-dark100_light900">{name}</h2>
-          <p className="paragraph-regular text-dark200_light800">@{username}</p>
-
-          <div className="mt-5 flex flex-wrap items-center justify-start gap-5">
-            {portfolio && (
-              <ProfileLink
-                imgUrl={"/icons/link.svg"}
-                href={portfolio}
-                title="Portfolio"
-              />
-            )}
-
-            {location && (
-              <ProfileLink imgUrl={"/icons/location.svg"} title="Location" />
-            )}
-
-            <ProfileLink
-              imgUrl={"/icons/calendar.svg"}
-              title={dayjs(createdAt).format("MMMM YYYY")}
-            />
-          </div>
-
-          {bio && (
-            <p className="paragraph-regular text-dark400_light800 mt-8">
-              {bio}
+          <div className="mt-3">
+            <h2 className="h2-bold text-dark100_light900">{name}</h2>
+            <p className="paragraph-regular text-dark200_light800">
+              @{username}
             </p>
+
+            <div className="mt-5 flex flex-wrap items-center justify-start gap-5">
+              {portfolio && (
+                <ProfileLink
+                  imgUrl={"/icons/link.svg"}
+                  href={portfolio}
+                  title="Portfolio"
+                />
+              )}
+
+              {location && (
+                <ProfileLink imgUrl={"/icons/location.svg"} title="Location" />
+              )}
+
+              <ProfileLink
+                imgUrl={"/icons/calendar.svg"}
+                title={dayjs(createdAt).format("MMMM YYYY")}
+              />
+            </div>
+
+            {bio && (
+              <p className="paragraph-regular text-dark400_light800 mt-8">
+                {bio}
+              </p>
+            )}
+          </div>
+        </div>
+
+        <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
+          {isCurrentUser && (
+            <Link href={"/profile/edit"}>
+              <Button className="paragraph-medium btn-secondary text-dark300_light900 min-h-12 min-w-44 cursor-pointer px-4 py-3">
+                Edit Profile
+              </Button>
+            </Link>
           )}
         </div>
-      </div>
+      </section>
 
-      <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
-        {isCurrentUser && (
-          <Link href={"/profile/edit"}>
-            <Button className="paragraph-medium btn-secondary text-dark300_light900 min-h-12 min-w-44 cursor-pointer px-4 py-3">
-              Edit Profile
-            </Button>
-          </Link>
-        )}
-      </div>
-    </section>
+      <Stats
+        totalQuestions={totalQuestions}
+        totalAnswers={totalAnswers}
+        badges={{ GOLD: 0, SILVER: 0, BRONZE: 0 }}
+      />
 
-    <Stats totalQuestions={totalQuestions} totalAnswers={totalAnswers} badges={{ GOLD: 0, SILVER: 0, BRONZE: 0 }} />
+      <section className="mt-10 flex gap-10">
+        <Tabs defaultValue="top-posts" className="flex-2">
+          <TabsList className="background-light800_dark400 min-h-10.5 p-1">
+            <TabsTrigger value="top-posts" className="tab">
+              Top Posts
+            </TabsTrigger>
+            <TabsTrigger value="answers" className="tab">
+              Answers
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="top-posts" className="mt-5 flex w-full flex-col gap-6">
+            List of Questions
+          </TabsContent>
+          <TabsContent value="answers" className="mt-5 flex w-full flex-col gap-6">List of Answers</TabsContent>
+        </Tabs>
+
+        <div className="flex w-full min-w-62.5 flex-1 flex-col max-lg:hidden">
+          <h3 className="h3-bold text-dark200_light900">Top Tech</h3>
+          <div className="mt-7 flex flex-col gap-4">
+            <p>List of Tags</p>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
